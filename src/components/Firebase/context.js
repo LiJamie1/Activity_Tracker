41e41416b418397278bcbe269fda9e30077299b6
Jamie <lijamie19828@gmail.com>
2022-03-15 23:34:04 -0700

    add documentation to context.js, create index to handle imports/exports

--- src/components/Firebase/context.js
+++ src/components/Firebase/context.js
@@ -1,5 +1,15 @@
 import React from "react";
 
+/* 
+  Takes in a component
+  Keeps its properties by spreading them out via {…props}
+  Consumes Firebase by using the Context API
+  Add the firebase class as an added property
+
+  https://reactjs.org/docs/higher-order-components.html
+  https://reactjs.org/docs/context.html
+*/
+
 const FirebaseContext = React.createContext(null);
 
 export const withFirebase = (Component) => (props) => {
